24d5e8a2bc2daeaac017174590117b536b048544
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-02 14:11:06 -0500

    Completed Spinning the Discs Week 4A Studio

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,10 +1,17 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc {
 
-    // TODO: Implement your custom interface.
+    public DVD (String aName, int maxCapacity, String aType, int someUsedCapacity) {
+        super(aName, maxCapacity, aType, someUsedCapacity);
+    }
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
+    public void spinDisc() {
+        System.out.println("A DVD spins at a rate of 570 - 1600 rpm.");
+    }
+
+    public void readData() {
+        System.out.println("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
 
 }
